6d4e46560dbd9f9d2e2bc40549d91b42f0507b31
Brittany Garrett <brittanymgarrett@gmail.com>
2021-11-30 21:35:02 -0600

    Updated

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -63,59 +63,5 @@ export const NavBar = () => {
       </Menu>
     </div>
 
-    // <ul className="navbar">
-    //   <li className="navbar__item">
-    //     <img className="navbar__logo" src={Logo} alt="logo img" />
-    //   </li>
-    //   <li className="navbar__item">
-    //     <Link className="navbar__link" to="/">
-    //       Posts
-    //     </Link>
-    //   </li>
-    //   <li className="navbar__item">
-    //     <Link className="navbar__link" to="/myposts">
-    //       My Posts
-    //     </Link>
-    //   </li>
-    //   <li className="navbar__item">
-    //     <Link className="navbar__link" to="/allusers">
-    //       All Users
-    //     </Link>
-    //   </li>
-    //   <li className="navbar__item">
-    //     <Link className="navbar__link" to="/profile">
-    //       Profile
-    //     </Link>
-    //   </li>
-    //   {/* <li className="navbar__item">
-    //             <Link className="navbar__link" to="/explore">Explore</Link>
-    //         </li> */}
-    //   {localStorage.getItem("rare_user_id") !== null ? (
-    //     <li className="nav-item">
-    //       <button
-    //         className="nav-link fakeLink"
-    //         onClick={() => {
-    //           localStorage.removeItem("rare_user_id");
-    //           history.push({ pathname: "/" });
-    //         }}
-    //       >
-    //         Logout
-    //       </button>
-    //     </li>
-    //   ) : (
-    //     <>
-    //       <li className="nav-item">
-    //         <Link className="nav-link" to="/login">
-    //           Login
-    //         </Link>
-    //       </li>
-    //       <li className="nav-item">
-    //         <Link className="nav-link" to="/register">
-    //           Register
-    //         </Link>
-    //       </li>
-    //     </>
-    //   )}
-    // </ul>
   );
 };
